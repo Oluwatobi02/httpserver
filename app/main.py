31b941d54c2cca200c702b6799f9eb3b56c27aa4
Oluwatobi <tobiolajide01@gmail.com>
2024-11-28 21:36:23 -0500

    [no message

--- app/main.py
+++ app/main.py
@@ -7,8 +7,11 @@ def main():
 
     # Uncomment this to pass the first stage
     #
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    while True:
+        server_socket = socket.create_server(("0.0.0.0", 4221), reuse_port=True)
+        conn, address = server_socket.accept() # wait for client
+        conn.sendall(b'HTTP/1.1 200 OK\r\n\r\n')
+
 
 
 if __name__ == "__main__":
